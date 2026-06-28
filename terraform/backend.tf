e2022c21f590a2c38b9d94b1c31f801f8d8d5c65
sushrith <sushrithazknl@outlook.com>
2026-06-27 23:16:21 -0400

    initial commit10

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "cloudtasktfstate12345"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "local" {}
 }
